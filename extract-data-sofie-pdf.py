8a88a549fffc83219525fea2f1c53a66dccf7587
rubbos <41753645+rubbos@users.noreply.github.com>
2024-09-30 00:22:49 +0200

    file management for quicker development

--- extract-data-sofie-pdf.py
+++ extract-data-sofie-pdf.py
@@ -9,13 +9,13 @@ import time
 start_time = time.time()
 
 # Load file
-file_path = "sofie_form_date.pdf"
+file_path = "pdf_examples/sofie_form_date.pdf"
 doc = convert_from_path(file_path)
 path, file_name = os.path.split(file_path)
 file_base_name, file_extension = os.path.splitext(file_name)
 
 # Check if file is extracted already to speedup testing
-if not os.path.isfile(f"extracted_{file_path}.txt"):
+if not os.path.isfile(f"{path}/extracted_{file_base_name}.txt"):
     # Convert to string with OCR.
     # Not using OpenCV because user can fill the document by hand and make pictures of it.
     text = ""
@@ -27,16 +27,16 @@ if not os.path.isfile(f"extracted_{file_path}.txt"):
     text = text.replace("\n", " ")
 
     # Save string to .txt for testing purposes
-    with open(f"extracted_{file_path}.txt", "w") as text_file:
+    with open(f"{path}/extracted_{file_base_name}.txt", "w") as text_file:
         text_file.write(text)
 
 # If file exists, skip the pytesseract to speedup testing
 else:
-    with open(f"extracted_{file_path}.txt", "r") as text_file:
+    with open(f"{path}/extracted_{file_base_name}.txt", "r") as text_file:
         text = text_file.read()
 
 
-# Extract the data between 2 keywords, refactor needed
+# Extract the data between 2 keywords if it exists
 def extract_text_between_keywords(text, start_keyword, end_keyword):
     try:
         start_index = text.index(start_keyword) + len(start_keyword)
@@ -45,6 +45,12 @@ def extract_text_between_keywords(text, start_keyword, end_keyword):
         return None
 
     return text[start_index:end_index].strip()
+
+
+def extract_multiple_text_between_keywords(text, start_keyword, end_keyword):
+    pattern = rf"{re.escape(start_keyword)}(.*?){re.escape(end_keyword)}"
+    matches = re.findall(pattern, text, re.DOTALL)
+    return matches
 
 
 # Testing purpose
@@ -113,7 +119,7 @@ working_date = find_date(working_date)
 working_date = [transform_date(date) for date in working_date]
 check_if_found(working_date, keyword_working_date)
 
-# Extract multiple place, country with dates
+# Extract place date(s)
 keyword_place_date = "Date from"
 place_date = extract_text_between_keywords(text, keyword_place_date, "Have you")
 place_date = find_date(place_date)
@@ -124,11 +130,14 @@ check_if_found(place_date, keyword_place_date)
 keyword_place_location = "upload it again"
 keyword_place_location_specified = "Place:"
 place_location = extract_text_between_keywords(text, keyword_place_location, "Have you")
-place_location = extract_text_between_keywords(
-    place_location, keyword_place_location_specified, "Date"
-)
-place_location.replace(":", "")
-place_location.replace("Country", "")
+
+place_location = place_location.replace(" ", "")
+place_location = place_location.replace(":", "")
+place_location = place_location.replace("Country", ", ")
+
+place_location = extract_multiple_text_between_keywords(place_location, "Place", "Date")
+
 check_if_found(place_location, keyword_place_location)
+
 # Runtime
 print("--- %s seconds ---" % (time.time() - start_time))
