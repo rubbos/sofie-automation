627f91dd9ff2f670c941321f106df42b5284b797
rubbos <41753645+rubbos@users.noreply.github.com>
2024-10-22 20:34:35 +0200

    refactoring

--- app/utils/reports.py
+++ app/utils/reports.py
@@ -4,63 +4,51 @@ import pandas as pd
 def create_main_report(df: pd.DataFrame, df2: pd.DataFrame) -> str:
     with open("temp_files/report.txt", "r") as file:
         report = file.read()
-
-    employer = df2.loc[df2["KEY"] == "Name employer", "VALUE"].values[0]
-    report = report.replace("[naam werkgever]", employer)
-    report = report.replace("&lt;naam werkgever&gt;", employer)
-
-    start_date = df2.loc[df2["KEY"] == "Date of entry into service", "VALUE"].values[0]
-    report = report.replace("&lt;datum aanvang dienstbetrekking&gt;", start_date)
-    report = report.replace("&lt;datum indiensttreding&gt;", start_date)
-    report = report.replace("&lt;datum aanvang tewerkstelling&gt;", start_date)
-
-    arrival_date = df.loc[df["KEY"] == "Arrival date", "VALUE"].values[0]
-    report = report.replace("&lt;datum aankomst NL&gt;", arrival_date)
-    report = report.replace("&lt;datum kwalificatie als werknemer&gt;", arrival_date)
-    report = report.replace("&lt;datum eerste werkdag in Nederland&gt;", arrival_date)
-
-    job_name = df2.loc[df2["KEY"] == "Job title", "VALUE"].values[0]
-    report = report.replace("&lt;naam functie&gt;", job_name)
-    ufo_code = df2.loc[df2["KEY"] == "UFO code", "VALUE"].values[0]
-    report = report.replace("&lt;eventueel functiecode vermelden&gt;", f"({ufo_code})")
+    
+    replacements = {
+        "[naam werkgever]": get_value(df2, "Name employer"),
+        "&lt;naam werkgever&gt;": get_value(df2, "Name employer"),
+        "&lt;datum aanvang dienstbetrekking&gt;": get_value(df2, "Date of entry into service"),
+        "&lt;datum indiensttreding&gt;": get_value(df2, "Date of entry into service"),
+        "&lt;datum aanvang tewerkstelling&gt;": get_value(df2, "Date of entry into service"),
+        "&lt;datum aankomst NL&gt;": get_value(df, "Arrival date"),
+        "&lt;datum kwalificatie als werknemer&gt;": get_value(df, "Arrival date"),
+        "&lt;datum eerste werkdag in Nederland&gt;": get_value(df, "Arrival date"),
+        "&lt;naam functie&gt;": get_value(df2, "Job title"),
+        "&lt;eventueel functiecode vermelden&gt;": f"({get_value(df2, "UFO code")})",
+    }
 
     # signed = df.loc[df["KEY"] == "Arrival date", "VALUE"].values[0]
     # report = report.replace("ADD SIGNED DATE ON AO", signed)
 
-    # wilsovereenkomst
-
+    report = replace_values(replacements, report)
     return report
-
 
 def create_email_report(df: pd.DataFrame, df2: pd.DataFrame) -> str:
     with open("temp_files/email.txt", "r") as file:
         report = file.read()
 
-    name = df.loc[df["KEY"] == "Full name", "VALUE"].values[0]
-    report = report.replace("{name}", name)
+    replacements = {
+        "{name}": get_value(df, "Full name"),
+        "{bsn}": get_value(df2, "BSN number"),
+        "{birth}": get_value(df2, "Date of birth"),
+        "{employer}": get_value(df2, "Name employer"),
+        "{lhm}": get_value(df2, "Loonheffing number"),
+        # "{start_date}": get_value(df, ""),
+        # "{end_date}": get_value(df, ""),
+        "{job_name}": get_value(df2, "Job title"),
+        # "{salarynorm}": get_value(df, "salarynorm")
+    }
+    report = replace_values(replacements, report)
+    return report
 
-    bsn = df2.loc[df2["KEY"] == "BSN number", "VALUE"].values[0]
-    report = report.replace("{bsn}", bsn)
 
-    date_of_birth = df2.loc[df2["KEY"] == "Date of birth", "VALUE"].values[0]
-    report = report.replace("{birth}", date_of_birth)
+def get_value(df: pd.DataFrame, key: str) -> str:
+    """Retrieve a value from the DataFrame based on the given key."""
+    return df.loc[df["KEY"] == key, "VALUE"].values[0]
 
-    employer = df2.loc[df2["KEY"] == "Name employer", "VALUE"].values[0]
-    report = report.replace("{employer}", employer)
 
-    loonheffing = df2.loc[df2["KEY"] == "Loonheffing number", "VALUE"].values[0]
-    report = report.replace("{lhm}", loonheffing)
-
-    # start_date_30 = df.loc[df["KEY"] == "Loonheffing number", "VALUE"].values[0]
-    # report = report.replace("{start_date}", start_date_30)
-
-    # end_date_30 = df.loc[df["KEY"] == "Loonheffing number", "VALUE"].values[0]
-    # report = report.replace("{end_date}", end_date_30)
-
-    job_name = df2.loc[df2["KEY"] == "Job title", "VALUE"].values[0]
-    report = report.replace("{job_name}", job_name)
-
-    # salarynorm = df.loc[df["KEY"] == "", "VALUE"].values[0]
-    # report = report.replace("{salarynorm}", salarynorm)
-
+def replace_values(replacements: dict, report: str):
+    for key, value in replacements.items():
+        report = report.replace(key, value)
     return report
