c953aead05baffb45027c69c46bffbb182d4d245
rubbos <41753645+rubbos@users.noreply.github.com>
2024-11-02 23:29:16 +0100

    added function and doctring to calcs

--- app/utils/calculations.py
+++ app/utils/calculations.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 
 def signed_outside_nl(signed_date: str, arrival_date: str) -> bool:
+    """If user signed the contract in the Netherlands, a wilsovereenkomst is needed"""
     date_format = "%d-%m-%Y"
     date1 = datetime.strptime(signed_date, date_format)
     date2 = datetime.strptime(arrival_date, date_format)
@@ -12,6 +13,12 @@ def signed_outside_nl(signed_date: str, arrival_date: str) -> bool:
 
 
 def salarynorm(ufo_code: str) -> str:
+    """If the users job position is an academic position, its an exception"""
     if ufo_code.startswith("01"):
         return "Wetenschappelijk O&O"
     return "Regulier"
+
+
+def start_date(application_upload_date: str, start_date, str) -> bool:
+    """If the difference between dates is less than 4 months, its an exception"""
+    ...
