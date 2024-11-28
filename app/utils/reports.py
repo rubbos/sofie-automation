fd6d1964df114993f4d2d95717aaa44eb7271920
rubbos <41753645+rubbos@users.noreply.github.com>
2024-11-28 22:58:23 +0100

    adds to report

--- app/utils/reports.py
+++ app/utils/reports.py
@@ -13,17 +13,29 @@ def regular_application(tax_form, application_form, employment_contract):
     ao_start_date = get_valuex(application_form, "ao_start_date")
     employer_type = get_valuex(application_form, "employer_type")
     first_work_date = get_valuex(tax_form, "first_work_date")
+    ao_signed_date = get_valuex(employment_contract, "ao_signed_date")
+    arrival_date = (get_valuex(tax_form, "arrival_date"),)
+    wo_signed_date = get_valuex(employment_contract, "wo_signed_date")
 
-    text1 = verslag_werknemer(employer, ao_start_date, employer_type, first_work_date)
-    text2 = verslag_aanwerving(
-        ao_start_date=ao_start_date,
-        ao_signed_date=get_valuex(employment_contract, "ao_signed_date"),
-        recent_location="abc",
-        arrival_date=get_valuex(tax_form, "arrival_date"),
-        wo_signed_date=get_valuex(employment_contract, "wo_signed_date"),
-        explain_wo="need text",
+    recent_location = "need text"
+    explain_wo = "need text"
+    recent_location_months = "need text"
+
+    title = formatting_text("Verslag regulier")
+    werknemer = verslag_werknemer(
+        employer, ao_start_date, employer_type, first_work_date
     )
-    return text1 + text2
+    aanwerving = verslag_aanwerving(
+        ao_start_date,
+        ao_signed_date,
+        recent_location,
+        arrival_date,
+        wo_signed_date,
+        explain_wo,
+    )
+    buitenland = verslag_buitenland(recent_location, recent_location_months)
+    woonplaats_radius = verslag_woonplaats_radius()
+    return title + werknemer + aanwerving + buitenland + woonplaats_radius
 
 
 def exception_change_of_employer(): ...
@@ -52,8 +64,8 @@ def verslag_aanwerving(
     ao_signed_date,
     recent_location,
     arrival_date,
-    wo_signed_date=False,
-    explain_wo=False,
+    wo_signed_date,
+    explain_wo,
 ):
     title = "Verslag aanwerving"
     optional_text = f"Eerder is er al een wilsovereenkomst tot stand gekomen op {wo_signed_date}. Dit blijkt uit: {explain_wo}."
@@ -61,9 +73,18 @@ def verslag_aanwerving(
     return formatting_text(title, main_text)
 
 
-def verslag_buitenland():
-    title = ""
-    text = f""
+def verslag_buitenland(recent_location, recent_location_months):
+    title = "Verslag 150 km criterium 16/24 maanden criterium"
+    text1 = f"Bij de aanwerving woonde werknemer in {recent_location}. De werknemer woonde daar ook gedurende {recent_location_months} van de 24 maanden voorafgaand aan de tewerkstelling. Deze woonplaats ligt op meer dan 150 km van de Nederlandse grens. Het CV en de informatie op het aanvraagformulier geven geen aanleiding om iets anders te concluderen."
+    text2 = f"Volgens het cv werkte/studeerde werknemer als ...&nbsp; in &lt;plaats/land&gt; voor de start van de tewerkstelling van &lt;datum&gt; tot &lt;datum&gt;. &lt;Geef indien mogelijk informatie over de activiteiten en plaats voor aankomst in Nederland"
+    text3 = f"Conclusie: het is aannemelijk dat werknemer op meer dan 150 km van de Nederlandse grens woonde gedurende meer dan 2/3 van de 24 maanden direct voorafgaand aan de eerste dag van tewerkstelling."
+    text = text1 + text2 + text3
+    return formatting_text(title, text)
+
+
+def verslag_woonplaats_radius():
+    title = "Foto woonplaats radius 150km"
+    return formatting_text(title)
 
 
 def verslag_deskundigheid():
@@ -76,8 +97,10 @@ def verslag_looptijd():
     text = f""
 
 
-def formatting_text(title, text):
-    return title + "<br>" + text + "<br><br>"
+def formatting_text(title, text=False):
+    if text:
+        return title + "<br>" + text + "<br><br>"
+    return title + "<br><br>"
 
 
 def create_main_report(
