9648a28d9b6404d9789e5a71826202a0748c4529
Ruben <41753645+rubbos@users.noreply.github.com>
2025-03-29 21:40:52 +0000

    removed unused func and small edit to report

--- app/utils/locations_table.py
+++ app/utils/locations_table.py
@@ -8,29 +8,15 @@ def create_table(data):
         start = datetime.strptime(start_date, fmt)
         end = datetime.strptime(end_date, fmt)
         delta = relativedelta(end, start)
-        return f"{delta.years * 12 + delta.months}m + {delta.days}d"
+        return f"{delta.years * 12 + delta.months} maanden + {delta.days} dagen"
 
     rows = []
     for entry in data:
         if isinstance(entry, list) and len(entry) >= 4:
             start_date, end_date, city, country = entry[:4]
             time_of_stay = calculate_time_of_stay(start_date, end_date)
-            row = f"<br>{city}, {country} van {start_date} t/m {end_date}({time_of_stay})."
+            row = f"<br> - {start_date} t/m {end_date} in {city}, {country} ({time_of_stay})."
             rows.append(row)
         else:
             print("Invalid entry:", entry)  # Debugging
     return "\n".join(rows)
-
-
-def convert_string_to_data(string_data):
-    # Remove surrounding brackets and split by rows
-    string_data = string_data.strip("[]")
-    rows = string_data.split("], [")
-
-    # Convert each row into a list
-    data = []
-    for row in rows:
-        row = row.strip("[]")
-        entries = row.split(", ")
-        data.append([entry.strip("'\"") for entry in entries])
-    return data
